3d0ef8eb7f79dfca08a9c410dd3031dbd622f5e2
BradleyGS1 <bradthegadd@gmail.com>
2024-07-15 04:49:49 +0100

    Commit updated to follow pep8 styling.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+
+
 import numpy as np
+
 
 def foo(x):
     return np.sum(np.arange(x+1))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
+
+
 from main import foo
+
 
 def test_foo_0():
     assert foo(3) > 5
 
 def test_foo_1():
     assert foo(4) < 10
+    
